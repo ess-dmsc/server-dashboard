aeff2b2ec5497a51eb0c1f014c21dbb0ae1e9e6f
Morten Jagd Christensen <morten.christensen@esss.se>
2017-02-15 11:40:47 +0100

    building graylog, h5cc, efu, data types, copying binaries

--- efu/build.bash
+++ efu/build.bash
@@ -10,7 +10,7 @@ function errexit()
   exit 1
 }
 
-PROJS="event-formation-unit streaming-data-types"
+PROJS="event-formation-unit streaming-data-types h5cc graylog-logger"
 
 function clone_projects()
 {
@@ -24,22 +24,52 @@ function clone_projects()
 
 function build_data_types()
 {
-  pushd streaming-data-types
-    mkdir build
-    pushd build
-      cmake .. || errexit "cmake failed for streaming-data-types"
-      make || errexit "make failed for streaming-data-types"
-      cp schemas/*.h $IDIR || errexit "cant copy schema header files"
-    popd
+  echo "Building streaming-data-types"
+  mkdir -p streaming-data-types/build || errexit "cant make build dir"
+  pushd streaming-data-types/build
+    cmake .. || errexit "cmake failed for streaming-data-types"
+    make || errexit "make failed for streaming-data-types"
+    cp schemas/*.h $IDIR || errexit "cant copy schema header files"
   popd
 }
 
 function build_efu()
 {
+  COPYFILES="cspec.so cspec2.so nmx.so udp.so gencspec gencspecfile gennmxfile efu2"
+
+  echo "Building event-formation-unit"
+  echo "NOKAFKA!"
   pushd event-formation-unit/prototype2
-    make || errexit "make failed for streaming-data-types"
-    cp *.so $ODIR || errexit "cant copy loadble instrument pipelines"
-    cp efu2 $ODIR || errexit "cant copy event formation exesutable"
+    make NOKAFKA=y HDF5=y V=y || errexit "make failed for EFU"
+    mkdir -p $ODIR/data || errexit "cant create data dir"
+    cp data/* $ODIR/data || errexit "cant copy data files"
+    for cpfile in $COPYFILES
+    do
+      echo "Copying "$cpfile
+      cp $cpfile $ODIR || errexit "cant copy $cpfile to output dir"
+    done
+  popd
+}
+
+function build_h5cc()
+{
+  echo "Building h5cc"
+  mkdir -p h5cc/build || errexit "cant create h5cc build dir"
+  pushd h5cc/build
+    cmake ../source || "cmake failed for h5cc"
+    make || errexit "make failed for h5cc"
+  popd
+}
+
+function build_graylog_logger()
+{
+  echo "Building graylog-logger"
+  pushd graylog-logger/graylog_logger
+    mkdir build
+    pushd build
+      cmake ..
+      make || errexit "make failed for graylog-logger"
+    popd
   popd
 }
 
@@ -55,6 +85,8 @@ mkdir output
 ODIR=$(pwd)/output
 clone_projects
 build_data_types
+build_h5cc
+build_graylog_logger
 build_efu
 
 $BANNER Done
