8cc4b4c6bed41375ed10860e1211bfc0625c4704
Morten Jagd Christensen <morten.christensen@esss.se>
2017-09-21 17:48:59 +0200

    ansible for IKON!

--- ikondemo/demoscripts/datagen_multigrid.bash
+++ ikondemo/demoscripts/datagen_multigrid.bash
@@ -12,7 +12,7 @@ function loadandrun()
   CALIB=$1
   RUN=$2
   $BASE/util/efushell/mgloadcal.py -i 10.4.0.215 -p 8000 $CALIB
-  $BASE/bin/mgcncsgenjson -b ~/ikondata/MG_CNCS -r allfiles.json -j $RUN -t $throttle -i 172.24.0.201 -p 9000
+  $BASE/bin/mgcncsgenjson -b ~/ikondata/MG_CNCS -r ~/dmg-build-scripts/ikondemo/demoscripts/allfiles.json -j $RUN -t $throttle -i 172.24.0.201 -p 9000
 }
 
 
