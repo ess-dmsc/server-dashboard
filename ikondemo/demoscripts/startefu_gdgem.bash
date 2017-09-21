7ea20f09ba814188cd0a0709563070210479c6c7
Morten Jagd Christensen <mortenjc@jcaps.com>
2017-09-21 21:43:25 +0200

    renamed files

--- ikondemo/demoscripts/startefu_gdgem.bash
+++ ikondemo/demoscripts/startefu_gdgem.bash
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-BASE=/home/mortenchristensen
+BASE=~/output
 
-export LD_LIBRARY_PATH=/opt/dm_group/usr/lib:$BASE/output/lib
+export LD_LIBRARY_PATH=/opt/dm_group/usr/lib:$BASE/lib
 
 BROKER=172.24.0.205:9092
 CARBON=10.4.0.216
 
-pushd $BASE/output/bin
+pushd $BASE/bin
 pwd
 ./efu2 -d gdgem -p 9001 -m 8001 -b $BROKER -g $CARBON -c -5 -f ~/dmg-build-scripts/ikondemo/demoscripts/nmx_config.json
 popd
--- ikondemo/demoscripts/startefu_multigrid.bash
+++ ikondemo/demoscripts/startefu_multigrid.bash
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-BASE=/home/mortenchristensen
+BASE=~/output
 
-export LD_LIBRARY_PATH=/opt/dm_group/usr/lib:$BASE/output/lib
+export LD_LIBRARY_PATH=/opt/dm_group/usr/lib:$BASE/lib
 
 BROKER=172.24.0.205:9092
 CARBON=10.4.0.216
 
-pushd $BASE/output/bin
+pushd $BASE/bin
 pwd
 ./efu2 -d mgcncs2 -p 9000 -m 8000 -b $BROKER -g $CARBON -c -5
 popd
