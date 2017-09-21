6893c393ade8df7977127a6119c5a8c980061e35
Morten Jagd Christensen <mortenchristensen@esss.se>
2017-09-21 16:51:44 +0200

    small fixes

--- ikondemo/demoscripts/datagen_gdgem.bash
+++ ikondemo/demoscripts/datagen_gdgem.bash
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-throttle={1:-0}
+throttle={1:-1}
 
-BASE=/home/mortenchristensen/output
+BASE=~/output
 
 export LD_LIBRARY_PATH=/opt/dm_group/usr/lib:$BASE/lib
 
 while [[ 1 ]]
 do
-  for filename in ../ikondata/GEM/*.h5
+  for filename in ~/ikondata/GEM/*.h5
   do
     $BASE/bin/gennmxfile -f $filename -p 9001 -i 172.24.0.201 -t $throttle
     sleep 5
--- ikondemo/demoscripts/datagen_multigrid.bash
+++ ikondemo/demoscripts/datagen_multigrid.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-throttle=$1
+throttle=${1:-10}
 
 BASE=~/output
 
@@ -12,8 +12,7 @@ function loadandrun()
   CALIB=$1
   RUN=$2
   $BASE/util/efushell/mgloadcal.py -i 10.4.0.215 -p 8000 $CALIB
-  #$BASE/bin/mgcncsgenfile -f ../detectordata/cncs_10_13.bin -t $throttle -i 172.24.0.201 -p 9000
-  $BASE/bin/mgcncsgenjson -b ~/detectordata -r allfiles.json -j ikon1 -t $throttle -i 172.24.0.201 -p 9000
+  $BASE/bin/mgcncsgenjson -b ~/ikondata/MG_CNCS -r allfiles.json -j $RUN -t $throttle -i 172.24.0.201 -p 9000
 }
 
 
