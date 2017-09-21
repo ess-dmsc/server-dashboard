d0192cc0a1fa129c01326fe9fcbbe4a545288501
Morten Jagd Christensen <morten.christensen@esss.se>
2017-09-21 11:05:45 +0200

    ikon scripts

--- ikondemo/demoscripts/datagen_multigrid.bash
+++ ikondemo/demoscripts/datagen_multigrid.bash
@@ -2,15 +2,24 @@
 
 throttle=$1
 
-BASE=/home/mortenchristensen/output
+BASE=~/output
 
 export LD_LIBRARY_PATH=/opt/dm_group/usr/lib:$BASE/lib
 
 
-$BASE/util/efushell/mgloadcal.py -i 10.4.0.215 -p 8000 /home/mortenchristensen/ikondemo/cncs_10_13
+function loadandrun()
+{
+  CALIB=$1
+  RUN=$2
+  $BASE/util/efushell/mgloadcal.py -i 10.4.0.215 -p 8000 $CALIB
+  #$BASE/bin/mgcncsgenfile -f ../detectordata/cncs_10_13.bin -t $throttle -i 172.24.0.201 -p 9000
+  $BASE/bin/mgcncsgenjson -b ~/detectordata -r allfiles.json -j ikon1 -t $throttle -i 172.24.0.201 -p 9000
+}
+
+
 
 while [[ 1  ]]
-do 
-   $BASE/bin/mgcncsgenfile -f ../detectordata/cncs_10_13.bin -t $throttle -i 172.24.0.201 -p 9000
-   sleep 5
+do
+   loadandrun ~/dmg-build-scripts/ikondemo/demoscripts/mgcalib/validruns_15 ikon1
+   loadandrun ~/dmg-build-scripts/ikondemo/demoscripts/mgcalib/validruns_15 ikon2
 done
--- ikondemo/demoscripts/multigrid.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-BASE=/home/mortenchristensen/output
-
-	pushd $BASE/bin
-   ../output/util/efushell/mgloadcal.py -p 8000 f
-popd
-
