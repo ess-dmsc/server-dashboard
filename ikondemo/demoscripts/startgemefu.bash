b1f4e88e10e973eeee5427157538c0e4ae6da6e9
MGS <martin.shetty@esss.se>
2017-09-21 16:30:39 +0200

    gem scripts updated

--- ikondemo/demoscripts/startgemefu.bash
+++ ikondemo/demoscripts/startgemefu.bash
@@ -9,5 +9,5 @@ CARBON=10.4.0.216
 
 pushd $BASE/output/bin
 pwd
-./efu2 -d gdgem -p 9001 -m 8001 -b $BROKER -g $CARBON -c -5 
+./efu2 -d gdgem -p 9001 -m 8001 -b $BROKER -g $CARBON -c -5 -f ~/dmg-build-scripts/ikondemo/demoscripts/nmx_config.json
 popd
