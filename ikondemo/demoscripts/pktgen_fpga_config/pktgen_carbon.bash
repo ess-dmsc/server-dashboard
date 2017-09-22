2375422086540e97eb4f66c8dafb9a314d2e9ae6
Morten Jagd Christensen <morten.christensen@esss.se>
2017-09-22 10:17:29 +0200

    adding full path to ethtool

--- ikondemo/demoscripts/pktgen_fpga_config/pktgen_carbon.bash
+++ ikondemo/demoscripts/pktgen_fpga_config/pktgen_carbon.bash
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-while [[ 1 ]] 
+ofile=/tmp/ikondemoethstats
+
+while [[ 1 ]]
 do
   date=$(date +%s)
-  ethtool -S ens1 > ethstats
-  rx_packets=$(grep rx_packets:  ethstats | awk -e '{print $2}')
-  rx_bytes=$(grep rx_bytes: ethstats | awk -e '{print $2}')
-  rx_packets_phy=$(grep rx_packets_phy ethstats | awk -e '{print $2}')
-  rx_bytes_phy=$(grep rx_bytes_phy ethstats | awk -e '{print $2}')
+  /usr/sbin/ethtool -S ens1 > $ofile
+
+  rx_packets=$(grep rx_packets:  $ofile | awk -e '{print $2}')
+  rx_bytes=$(grep rx_bytes: $ofile | awk -e '{print $2}')
+  rx_packets_phy=$(grep rx_packets_phy $ofile | awk -e '{print $2}')
+  rx_bytes_phy=$(grep rx_bytes_phy $ofile | awk -e '{print $2}')
+
   echo "efu2.100g.rx_bytes ${rx_bytes} $date"
   echo "efu2.100g.rx_packets ${rx_packets} $date"
   echo "efu2.100g.rx_bytes_phy ${rx_bytes_phy} $date"
@@ -17,5 +21,6 @@ do
   echo "efu2.100g.rx_packets ${rx_packets} $date" | nc 10.4.0.216 2003
   echo "efu2.100g.rx_bytes_phy ${rx_bytes_phy} $date" | nc 10.4.0.216 2003
   echo "efu2.100g.rx_packets_phy ${rx_packets_phy} $date" | nc 10.4.0.216 2003
-  sleep 0.98 
+  rm -f $ofile
+  sleep 0.98
 done
