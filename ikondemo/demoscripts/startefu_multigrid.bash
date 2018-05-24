2a4493676806c1d09fa187c678b06895aa1ccfc4
Morten Jagd Christensen <morten.christensen@esss.se>
2018-05-24 16:19:42 +0200

    add relative path to modules, cncs renamed to cncs2

--- ikondemo/demoscripts/startefu_multigrid.bash
+++ ikondemo/demoscripts/startefu_multigrid.bash
@@ -8,5 +8,5 @@ BROKER=172.24.0.205:9092
 CARBON=10.4.0.216
 
 pushd $BASE/bin
-  ./efu -d $BASE/modules/mgcncs -p 9000 -m 8000 -b $BROKER -g $CARBON -c -5
+  ./efu -d ../modules/mgcncs2 -p 9000 -m 8000 -b $BROKER -g $CARBON -c -5
 popd
