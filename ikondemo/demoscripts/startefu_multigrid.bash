6658518e2610410a29456469e9c783f7a4b5c513
Afonso Mukai <28659574+amues@users.noreply.github.com>
2019-03-19 10:18:43 +0100

    Restore multigrid file

---
+++ ikondemo/demoscripts/startefu_multigrid.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+BASE=~/deployment/event-formation-unit
+
+export LD_LIBRARY_PATH=$BASE/lib
+
+BROKER=172.24.0.207:9092
+CARBON=10.4.0.216
+
+pushd $BASE/bin
+  ./efu -d ../modules/mgcncs2 --min_mtu 1500 -p 9000 -m 8000 -b $BROKER -g $CARBON -c -5
+popd
