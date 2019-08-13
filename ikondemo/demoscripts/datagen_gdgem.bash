43a6e413aec317fd00983ad6fda5742f360f28a4
Afonso Mukai <28659574+amues@users.noreply.github.com>
2019-08-13 11:35:49 +0200

    Update path for gdgem gen

--- ikondemo/demoscripts/datagen_gdgem.bash
+++ ikondemo/demoscripts/datagen_gdgem.bash
@@ -8,9 +8,6 @@ export LD_LIBRARY_PATH=$BASE/lib
 
 while [[ 1 ]]
 do
-  for filename in ~/ikondata/gdgem/a10000
-  do
-    $BASE/bin/nmxgen_hits -f $filename -p 9001 -i 172.24.0.205 -t $throttle
-    sleep 5
-  done
+  $BASE/bin/nmxgen_hits -f /mnt/data/EFU_reference/gdgem/2018_11/hits/a10000 -p 9001 -i 172.24.0.205 -t $throttle
+  sleep 5
 done
