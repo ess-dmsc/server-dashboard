3c6a31cfe7b6c44f64af6eda243c9687807bd419
Afonso Mukai <28659574+amues@users.noreply.github.com>
2019-08-13 11:03:40 +0200

    Update multigrid script

--- ikondemo/demoscripts/startefu_multigrid.bash
+++ ikondemo/demoscripts/startefu_multigrid.bash
@@ -4,9 +4,9 @@ BASE=~/deployment/event-formation-unit
 
 export LD_LIBRARY_PATH=$BASE/lib
 
-BROKER=172.24.0.207:9092
+BROKER=172.24.0.207:9094
 CARBON=172.30.242.21
 
 pushd $BASE/bin
-  ./efu -d ../modules/mgcncs2 --min_mtu 1500 -p 9000 -m 8000 -b $BROKER -g $CARBON -c -5
+  ./efu -d ../modules/mgmesytec -f /mnt/data/EFU_reference/multigrid/2018_09_03/Sequoia_mappings.json --nohwcheck -p 9000 -m 8000 -b $BROKER -g $CARBON
 popd
