fb94817147ded8c0fd95604f5100f820ad37cb81
Morten Jagd Christensen <morten.christensen@esss.se>
2019-09-02 16:46:55 +0200

    using new json configuration format

--- ikondemo/demoscripts/startefu_multigrid.bash
+++ ikondemo/demoscripts/startefu_multigrid.bash
@@ -9,5 +9,5 @@ CARBON=172.30.242.21
 GRAYLOG=172.30.242.21
 
 pushd $BASE/bin
-  ./efu -d ../modules/mgmesytec -f /mnt/data/EFU_reference/multigrid/2018_09_03/Sequoia_mappings.json --nohwcheck -p 9000 -m 8000 -b $BROKER -g $CARBON -a $GRAYLOG
+  ./efu -d ../modules/mgmesytec -f /mnt/data/EFU_reference/multigrid/2018_09_03/Sequoia_mappings2.json --nohwcheck -p 9000 -m 8000 -b $BROKER -g $CARBON -a $GRAYLOG
 popd
