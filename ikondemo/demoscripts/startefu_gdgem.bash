3fec45c9ab0427a9cab0fb241c70c52b4fb2474b
Morten Hilker-Skaaning <morten.hilkerskaaning@esss.se>
2020-01-21 11:03:16 +0100

    newer data for gdgem example

--- ikondemo/demoscripts/startefu_gdgem.bash
+++ ikondemo/demoscripts/startefu_gdgem.bash
@@ -9,5 +9,6 @@ CARBON=172.30.242.21
 GRAYLOG=172.30.242.21
 
 pushd $BASE/bin
-  ./efu -d ../modules/gdgem --nohwcheck -p 9001 -m 8001 -b $BROKER -g $CARBON -c -5 -f /mnt/data/EFU_reference/gdgem/2018_11/readouts/config.json -a $GRAYLOG
+  #./efu -d ../modules/gdgem --nohwcheck -p 9001 -m 8001 -b $BROKER -g $CARBON -c -5 -f /mnt/data/EFU_reference/gdgem/2018_11/readouts/config.json -a $GRAYLOG
+  ./efu -d ../modules/gdgem --nohwcheck -p 9001 -m 8001 -b $BROKER -g $CARBON -c -5 -f /mnt/data/EFU_reference/gdgem/2019_09/readouts/config.json -a $GRAYLOG
 popd
