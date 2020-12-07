554fe89c8dfac52b28efdc00af9607ba19d5ff99
Morten Jagd Christensen <morten.christensen@esss.se>
2020-12-07 15:54:52 +0100

    added loki data generator (pcap)

---
+++ ikondemo/demoscripts/datagen_loki.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+throttle=50000
+
+BASE=~/deployment/event-formation-unit
+
+export LD_LIBRARY_PATH=$BASE/lib
+
+while [[ 1 ]]
+do
+   $BASE/bin/udpgen_pcap -t $throttle -f /mnt/data/EFU_reference/loki/2020/wireshark/loki_data_301120.pcapng -i 172.24.0.205 -p 9021
+done
