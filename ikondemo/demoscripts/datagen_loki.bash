34d3d302751930dbb4dd717649aeab8c161e72ea
Morten Jagd Christensen <morten.christensen@esss.se>
2020-12-07 16:41:35 +0100

    add sleep between pcap files for loki

--- ikondemo/demoscripts/datagen_loki.bash
+++ ikondemo/demoscripts/datagen_loki.bash
@@ -9,4 +9,5 @@ export LD_LIBRARY_PATH=$BASE/lib
 while [[ 1 ]]
 do
    $BASE/bin/udpgen_pcap -t $throttle -f /mnt/data/EFU_reference/loki/2020/wireshark/loki_data_301120.pcapng -i 172.24.0.205 -p 9021
+   sleep 3
 done
