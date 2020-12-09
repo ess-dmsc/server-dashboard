422ff53bc11a662672c3265def07bbc0620a19b2
Morten Jagd Christensen <morten.christensen@esss.se>
2020-12-09 10:30:56 +0100

    misspelling for data dream data generator

--- ikondemo/demoscripts/datagen_dream.bash
+++ ikondemo/demoscripts/datagen_dream.bash
@@ -8,5 +8,5 @@ export LD_LIBRARY_PATH=$BASE/lib
 
 while [[ 1 ]]
 do
-   $BASE/bin/udpgen_jaloisue -t $throttle -f /mnt/data/EFU_reference/jalousie/2019_07/noise.bin -i 172.24.0.205 -p 9031
+   $BASE/bin/udpgen_jalousie -t $throttle -f /mnt/data/EFU_reference/jalousie/2019_07/noise.bin -i 172.24.0.205 -p 9031
 done
