9f9dbabae3495e8f25460fca5321bbc1c5d0b222
Morten Jagd Christensen <morten.christensen@esss.se>
2020-12-23 00:08:41 +0100

    updated pcap file for LoKI

--- ikondemo/demoscripts/datagen_loki.bash
+++ ikondemo/demoscripts/datagen_loki.bash
@@ -8,5 +8,5 @@ export LD_LIBRARY_PATH=$BASE/lib
 
 while [[ 1 ]]
 do
-   $BASE/bin/udpgen_pcap -t $throttle -f /mnt/data/EFU_reference/loki/2020/wireshark/loki_data_301120.pcapng -i 172.24.0.205 -p 9021
+   $BASE/bin/udpgen_pcap -t $throttle -f /mnt/data/EFU_reference/loki/2020/wireshark/loki_data_181220.pcapng -i 172.24.0.205 -p 9021
 done
