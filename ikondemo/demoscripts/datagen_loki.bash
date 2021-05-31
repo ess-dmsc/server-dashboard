c996ba850f76dcc2cca5677f2ca85eb73738ab4a
Morten Jagd Christensen <mortenjc@jcaps.com>
2021-05-31 10:31:41 +0200

    new loki data

--- ikondemo/demoscripts/datagen_loki.bash
+++ ikondemo/demoscripts/datagen_loki.bash
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-throttle=50000
+throttle=1000
 
 BASE=~/deployment/event-formation-unit
 
-data1=/mnt/data/EFU_reference/loki/2020/wireshark/loki_data_181220.pcapng
-data2=/mnt/data/EFU_reference/loki/2021_01/raw/mask.dat
+data1=/mnt/data/EFU_reference/loki/2021_05/mjc_test_II.pcap
 
 export LD_LIBRARY_PATH=$BASE/lib
 
 while [[ 1 ]]
 do
-   #$BASE/bin/udpgen_pcap -t $throttle -f $data1  -i 172.24.0.221 -p 9021
-   $BASE/bin/udpgen_lokiraw -t $throttle -f $data2  -i 172.24.0.221 -p 9021
+   $BASE/bin/udpgen_pcap -t $throttle -f $data1  -i 172.24.0.221 -p 9021
 done
